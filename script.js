4dd493dcf894cfe2514283692178b74d8f95449a
Orisabiyi <orisabiyidavid@gmail.com>
2024-01-22 18:02:19 +0100

    .

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 'use strict';
 const cartValue = document.querySelector('.cart__value');
+const header = document.querySelector('.header');
+const headerCartMain = document.querySelector('.header__cart-main');
 
 let curNum = 0;
 cartValue.textContent = curNum;
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const [taskItem] = JSON.parse(localStorage.getItem('task'));
+  renderListItem(taskItem)
+});
+
+const saveToLocalstorage = function (number) {
+  const task = [number];
+  localStorage.setItem('task', JSON.stringify(task));
+}
+
+const renderListItem = function (currentNum, currentPrice = 125) {
+  headerCartMain.innerHTML = '';
+  const html = `
+      <div class="header__cart-item">
+        <div class="header__cart-img--1">
+          <img src="images/image-product-1-thumbnail.jpg" alt="sneakers image" class="header__cart-icon header__cart-icon--3">
+        </div>
+        <p class="header__cart-description">Fall Limited Edition Sneakers $${currentPrice.toFixed(2)} x ${currentNum} <strong>$${(currentPrice * currentNum).toFixed(2)}</strong></p>
+        <div class="header__cart-img--2">
+          <img src="images/icon-delete.svg" alt="delete icon" class="header__cart-icon">
+        </div>
+      </div>
+      
+      <button class="btn btn--1">Checkout</button>`;
+
+  headerCartMain.insertAdjacentHTML('afterbegin', html);
+  headerCartMain.style.justifyContent = 'space-between';
+}
+
 
 // 0%, 100%, 200%, 300%
 const transformFigure = function (value) {
@@ -63,27 +95,6 @@ const increaseAndDecreaseNumber = function () {
 
 increaseAndDecreaseNumber();
 
-const renderListItem = function (currentNum, currentPrice = 125) {
-  const headerCartMain = document.querySelector('.header__cart-main');
-
-  headerCartMain.innerHTML = '';
-  const html = `
-      <div class="header__cart-item">
-        <div class="header__cart-img--1">
-          <img src="images/image-product-1-thumbnail.jpg" alt="sneakers image" class="header__cart-icon header__cart-icon--3">
-        </div>
-        <p class="header__cart-description">Fall Limited Edition Sneakers $${currentPrice.toFixed(2)} x ${currentNum} <strong>$${(currentPrice * currentNum).toFixed(2)}</strong></p>
-        <div class="header__cart-img--2">
-          <img src="images/icon-delete.svg" alt="delete icon" class="header__cart-icon">
-        </div>
-      </div>
-      
-      <button class="btn btn--1">Checkout</button>`;
-
-  headerCartMain.insertAdjacentHTML('afterbegin', html);
-  headerCartMain.style.justifyContent = 'space-between';
-}
-
 const addToCart = function () {
   const cartBtn = document.querySelector('.cart__btn');
   const headerCartPopup = document.querySelector('.header__cart-popup');
@@ -95,7 +106,33 @@ const addToCart = function () {
 
     // render List of item
     renderListItem(curNum);
+
+    // storage
+    saveToLocalstorage(curNum);
   })
 }
 
 addToCart();
+
+const deleteCartItem = function () {
+  headerCartMain.addEventListener('click', function (e) {
+    const target = e.target.classList.contains('header__cart-icon')
+    if (target) {
+      e.target.closest('.header__cart-item').remove();
+      document.querySelector('.btn--1').remove();
+      localStorage.clear();
+    }
+  })
+}
+
+deleteCartItem();
+
+const displayBasket = function () {
+  header.addEventListener('click', function (e) {
+    const target = e.target.closest('.header__cart-img');
+    if (!target) return;
+    e.currentTarget.querySelector('.header__cart-basket').classList.toggle('hidden');
+  })
+}
+
+displayBasket();
