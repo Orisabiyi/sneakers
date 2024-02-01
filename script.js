abd01a2e37b6783d68e2e5f6df4891d9b2c67ab2
Orisabiyi <orisabiyidavid@gmail.com>
2024-02-01 16:15:36 +0100

    .

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ const header = document.querySelector('.header');
 const headerCartPopup = document.querySelector('.header__cart-popup');
 const headerCartMain = document.querySelector('.header__cart-main');
 const cartValue = document.querySelector('.cart__value');
+const cartCount = document.querySelector('.cart__count');
+const cartBtn = document.querySelector('.cart__btn');
+
+
+const sliderList = document.querySelector('.slider__list');
+const sliderItem = document.querySelectorAll('.slider__item');
+const slider = document.querySelector('.slider');
+
+const overlay = document.querySelector('.overlay');
+const overlaySlides = document.querySelectorAll('.overlay__figure');
 
 let curNum = 0;
 cartValue.textContent = curNum;
+
+let curSlide = 0;
+const totalSlide = overlaySlides.length - 1;
 
 const saveToLocalstorage = function (number) {
   const task = [number];
@@ -23,9 +36,7 @@ const getItemLocalStorage = function () {
 }
 
 const loadPage = function () {
-  document.addEventListener('DOMContentLoaded', () => {
-    getItemLocalStorage();
-  })
+  getItemLocalStorage();
 }
 
 // 0%, 100%, 200%, 300%
@@ -38,53 +49,38 @@ const transformImg = function (className, value) {
 }
 
 // Display Image on Click
-const displayImageOnClick = function (figureClass, listClass, itemClass) {
-  const list = document.querySelector(`.${listClass}`);
-  const item = document.querySelectorAll(`.${itemClass}`);
+const displayImageOnClick = function (e) {
+  const active = e.target.classList.contains('slider__item--active');
+  if (active || e.target.classList.contains('slider__list')) return;
 
-  list.addEventListener('click', function (e) {
-    const itemActive = e.target.closest(`.${itemClass}`)?.classList.contains(`${itemClass}--active`);
-    const clickedItem = e.target.closest(`.${itemClass}`);
+  // remove existing active class from element
+  sliderItem.forEach(item => item.classList.remove('slider__item--active'));
 
-    // if active class is present
-    if (itemActive) return;
-
-    // Removing existing active class
-    item.forEach(item => item.classList.remove(`${itemClass}--active`));
-
-    // Adding active class to clicked item and image
-    clickedItem?.classList.add(`${itemClass}--active`);
-
-    // Transform to product image
-    transformImg(figureClass, -clickedItem?.dataset.id);
-  })
+  // Add active class to target element
+  e.target.classList.add('slider__item--active');
+  curSlide = +e.target.dataset.id;
+  transformImg('slider__figure', -curSlide);
 }
 
 // Increase and Decrease Number
-const plusAndMinus = function () {
-  const cartCount = document.querySelector('.cart__count');
+const increaseDecreaseNumber = function (e) {
+  if (e.target.classList.contains('cart__icon--1')) {
+    curNum--;
+    if (curNum < 0) curNum = 0;
+    cartValue.textContent = curNum;
+  }
 
-  cartCount.addEventListener('click', function (e) {
-    if (e.target.classList.contains('cart__icon--1')) {
-      curNum--;
-      if (curNum < 0) curNum = 0;
-      cartValue.textContent = curNum;
-    }
-
-    if (e.target.classList.contains('cart__icon--2')) {
-      curNum++;
-      cartValue.textContent = curNum;
-    }
-  })
+  if (e.target.classList.contains('cart__icon--2')) {
+    curNum++;
+    cartValue.textContent = curNum;
+  }
 }
 
-// Cart Basket
-const showBasket = function () {
-  header.addEventListener('click', function (e) {
-    const target = e.target.closest('.header__cart-img');
-    if (!target) return;
-    e.currentTarget.querySelector('.header__cart-basket').classList.toggle('hidden');
-  })
+const showBasket = function (e) {
+  const target = e.target.closest('.header__cart-img');
+
+  if (!target) return;
+  e.currentTarget.querySelector('.header__cart-basket').classList.toggle('hidden');
 }
 
 const renderListItem = function (currentNum, currentPrice = 125) {
@@ -109,75 +105,53 @@ const renderListItem = function (currentNum, currentPrice = 125) {
   headerCartMain.style.justifyContent = 'space-between';
 }
 
-const addToCart = function () {
-  const cartBtn = document.querySelector('.cart__btn');
+const addToCart = function (e) {
 
-  cartBtn.addEventListener('click', function (e) {
-    if (curNum === 0) return;
-    headerCartPopup.classList.remove('hidden');
-    headerCartPopup.textContent = curNum;
+  if (curNum === 0) return;
+  headerCartPopup.classList.remove('hidden');
+  headerCartPopup.textContent = curNum;
 
-    // render List of item
-    renderListItem(curNum);
-
-    // storage
-    saveToLocalstorage(curNum);
-  })
+  // Render List and Save to Storage
+  renderListItem(curNum);
+  saveToLocalstorage(curNum);
 }
 
-const deleteCartItem = function () {
-  headerCartMain.addEventListener('click', function (e) {
-    const target = e.target.classList.contains('header__cart-icon')
-    if (target) {
-      e.target.closest('.header__cart-item').remove();
-      document.querySelector('.btn--1').remove();
+const deleteCartItem = function (e) {
+  const target = e.target.classList.contains('header__cart-icon')
+  if (!target) return;
 
-      // Clear Local Storage
-      localStorage.clear();
-      headerCartPopup.textContent = 0;
-      headerCartPopup.classList.add('hidden');
-    }
-  })
+  e.target.closest('.header__cart-item').remove();
+  document.querySelector('.btn--1').remove();
+
+  // Clear Local Storage
+  localStorage.clear();
+  headerCartPopup.textContent = 0;
+  headerCartPopup.classList.add('hidden');
 }
 
-const overlay = document.querySelector('.overlay');
-
-const closeOverlayOnClick = function () {
-  if (overlay.classList.contains('hidden')) return;
-
-  overlay.addEventListener('click', function (e) {
-    if (!e.target.closest('.btn__close')) return;
-
-    overlay.classList.add('hidden');
-  })
+const closePopup = function (e) {
+  if (!e.target.closest('.btn__close')) return;
+  overlay.classList.add('hidden');
 }
 
-const displayOverlay = function () {
-  const slider = document.querySelector('.slider');
+const displayPopup = function (e) {
+  if (!e.target.closest('.slider__product')) return;
 
-  slider.addEventListener('click', function (e) {
-    if (!e.target.closest('.slider__product')) return;
-
-    overlay.classList.remove('hidden');
-    displayImageOnClick('overlay__figure', 'overlay__list', 'overlay__item');
-    closeOverlayOnClick();
-  })
+  overlay.classList.remove('hidden');
 }
 
-const overlaySliderControl = function () {
-  const figure = document.querySelectorAll('.overlay__figure');
-}
-
-overlaySliderControl();
-
-loadPage();
-addToCart();
-showBasket();
-plusAndMinus();
-deleteCartItem();
 
 transformImg('slider__figure', 0);
 transformImg('overlay__figure', 0);
 
-displayImageOnClick('slider__figure', 'slider__list', 'slider__item');
-displayOverlay();
+// Event Listener
+document.addEventListener('DOMContentLoaded', loadPage);
+
+cartBtn.addEventListener('click', addToCart);
+header.addEventListener('click', showBasket);
+headerCartMain.addEventListener('click', deleteCartItem);
+sliderList.addEventListener('click', displayImageOnClick);
+cartCount.addEventListener('click', increaseDecreaseNumber);
+
+overlay.addEventListener('click', closePopup);
+slider.addEventListener('click', displayPopup);
